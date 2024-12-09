e5240f4d89e33ad880c4fbca299ac18b89552d33
OlivierWienholtz <98882516+OlivierWienholtz@users.noreply.github.com>
2024-12-09 11:50:46 +0100

    move to .F90 for Statis Analyser Compatibility

--- engine/source/modules/user_interface_mod.F90
+++ engine/source/modules/user_interface_mod.F90
@@ -1,25 +1,25 @@
-Copyright>        OpenRadioss
-Copyright>        Copyright (C) 1986-2024 Altair Engineering Inc.
-Copyright>
-Copyright>        This program is free software: you can redistribute it and/or modify
-Copyright>        it under the terms of the GNU Affero General Public License as published by
-Copyright>        the Free Software Foundation, either version 3 of the License, or
-Copyright>        (at your option) any later version.
-Copyright>
-Copyright>        This program is distributed in the hope that it will be useful,
-Copyright>        but WITHOUT ANY WARRANTY; without even the implied warranty of
-Copyright>        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-Copyright>        GNU Affero General Public License for more details.
-Copyright>
-Copyright>        You should have received a copy of the GNU Affero General Public License
-Copyright>        along with this program.  If not, see <https://www.gnu.org/licenses/>.
-Copyright>
-Copyright>
-Copyright>        Commercial Alternative: Altair Radioss Software
-Copyright>
-Copyright>        As an alternative to this open-source version, Altair also offers Altair Radioss
-Copyright>        software under a commercial license.  Contact Altair to discuss further if the
-Copyright>        commercial version may interest you: https://www.altair.com/radioss/.
+!Copyright>        OpenRadioss
+!Copyright>        Copyright (C) 1986-2024 Altair Engineering Inc.
+!Copyright>
+!Copyright>        This program is free software: you can redistribute it and/or modify
+!Copyright>        it under the terms of the GNU Affero General Public License as published by
+!Copyright>        the Free Software Foundation, either version 3 of the License, or
+!Copyright>        (at your option) any later version.
+!Copyright>
+!Copyright>        This program is distributed in the hope that it will be useful,
+!Copyright>        but WITHOUT ANY WARRANTY; without even the implied warranty of
+!Copyright>        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!Copyright>        GNU Affero General Public License for more details.
+!Copyright>
+!Copyright>        You should have received a copy of the GNU Affero General Public License
+!Copyright>        along with this program.  If not, see <https://www.gnu.org/licenses/>.
+!Copyright>
+!Copyright>
+!Copyright>        Commercial Alternative: Altair Radioss Software
+!Copyright>
+!Copyright>        As an alternative to this open-source version, Altair also offers Altair Radioss
+!Copyright>        software under a commercial license.  Contact Altair to discuss further if the
+!Copyright>        commercial version may interest you: https://www.altair.com/radioss/.
       !||====================================================================
       !||    user_interface_mod      ../engine/share/modules/user_interface_mod.F
       !||--- called by ------------------------------------------------------
@@ -44,15 +44,18 @@ Copyright>        commercial version may interest you: https://www.altair.com/ra
       !||    sensor_mod              ../engine/share/modules/sensor_mod.F
       !||    user_windows_mod        ../common_source/modules/user_windows_mod.F
       !||====================================================================
-      MODULE USER_INTERFACE_MOD
-         USE USER_WINDOWS_MOD
-         USE SENSOR_MOD
-#include "my_real.inc"
-C-----------------------------------------------------------------------------------
-C     MODULE dedicated to pass arrays from RADIOSS to user routines.
-C-----------------------------------------------------------------------------------
-        
-        TYPE (USER_WINDOWS_) :: USER_WINDOWS
-        TYPE (SENSORS_)      :: SENSORS
+!! \brief  Module intent to provide common buffers for user interface callback
+      !||====================================================================
 
-      END MODULE USER_INTERFACE_MOD
+      module user_interface_mod
+         use user_windows_mod
+         use sensor_mod
+#include "my_real.inc"
+!-----------------------------------------------------------------------------------s
+!     module dedicated to pass arrays from radioss to user routines.
+!-----------------------------------------------------------------------------------
+        
+        type (user_windows_) :: user_windows
+        type (sensors_)      :: sensors
+
+      end module user_interface_mod
